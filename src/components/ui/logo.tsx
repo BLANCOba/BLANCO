c895aff7d629dbe7ad816e3b338323661a4f7170
Alexander Bittermann <alexander.bittermann@gmx.at>
2025-06-06 03:03:55 +0900

    updated components removed unused components and dependencies to reduce bundle size

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,23 +1,22 @@
 'use client';
 
-import { useTheme } from 'next-themes';
-import { motion } from 'framer-motion';
+import {motion} from 'framer-motion';
+import Image from "next/image";
+import logo from "@/../public/blancomedia_logo_white.png";
 
 export function Logo() {
-  const { theme } = useTheme();
-
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-      className="shrink-0"
-    >
-      <img
-          src="/blancomedia_logo_white.png"
-        alt="Blanco"
-        className="h-8 w-auto"
-      />
-    </motion.div>
-  );
+    return (
+        <motion.div
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{duration: 0.5}}
+            className="shrink-0"
+        >
+            <Image
+                src={logo}
+                alt="Blanco"
+                className="h-8 w-auto"
+            />
+        </motion.div>
+    );
 }
